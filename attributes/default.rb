71fbca08a0e7ce07d66c665775669f480b73a0a1
bipinbachhao <bipinbachhao@gmail.com>
2018-03-24 18:40:50 -0400

    Tested code

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['oracle']['rdbms']['ora_home'] = '/tmp/ora_home'
+default['oracle']['rdbms']['dbs'] = 'oracle_db1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+directory "#{node['oracle']['rdbms']['ora_home']}/network/admin" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+template "#{node['oracle']['rdbms']['ora_home']}/network/admin/listener.ora" do
+  source 'listener.erb'
+  owner 'root'
+  group 'root'
+end
